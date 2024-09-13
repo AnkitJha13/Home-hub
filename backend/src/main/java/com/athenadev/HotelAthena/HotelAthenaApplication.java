369e6ed9f1fae61a2f35e47ac6d0b9c29e511363
Ankit <ankitjha1403@gmail.com>
2024-09-13 20:20:07 +0530

    first commit

--- backend/src/main/java/com/athenadev/HotelAthena/HotelAthenaApplication.java
+++ backend/src/main/java/com/athenadev/HotelAthena/HotelAthenaApplication.java
@@ -1,5 +1,6 @@
 package com.athenadev.HotelAthena;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HotelAthenaApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.load();
 		SpringApplication.run(HotelAthenaApplication.class, args);
 		System.out.println("hello athena");
 	}
